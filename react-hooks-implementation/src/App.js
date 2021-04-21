835c70a7dcd92f6c6e413fd424323274fd39a215
shubham kushwaha <shubhkush1234@gmail.com>
2021-04-21 13:15:57 +0530

    added toggle useState

--- react-hooks-implementation/src/App.js
+++ react-hooks-implementation/src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+
+  const [ count, setCount ] = useState(0);
+  const [ isOn, setIsOn ] = useState(false);
+
+  // equivalent of 
+  // state={
+  //   count: 0
+  // }
+
+
+  // const incrementCount = () => {
+  //   this.setState(previousState => ({
+  //     count: previousState.count +1
+  //   }))
+  // };
+
+  const incrementCount = () => {
+    //updater function need not to return object.
+    setCount(previousCount => previousCount + 1);
+  }
+
+const toggleLight = () =>{(
+  setIsOn(previousIsOn  => !previousIsOn)
+)
+  
+}
+
+  // toggleLight = () => {
+  //   this.setState(
+  //     previousState => (
+  //       { isOn : !previousState.isOn }
+  //     )
+  //   )
+  // };
+
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div>
+      <button type="button" onClick={incrementCount}>I am clicked {count} times.</button>
+      {/* <button type="button" onClick={this.incrementCount}>I was clicked {this.state.count} times</button> */}
+      <h3>Toggle Light</h3>
+      {/* <div
+        style={{
+          height:"50px",
+          width:"50px",
+          background: this.state.isOn ? "yellow" : "grey"
+        }}
+        onClick={this.toggleLight}
+      ></div> */}
+      <div
+        style={{
+          height:"100px",
+          width:"100px",
+          background: isOn ? "yellow" : "grey"
+        }}
+        onClick={toggleLight}
+      >Click me to change color</div> 
+      <div>
+
+      </div>
     </div>
-  );
+    </> 
+  )
 }
 
 export default App;
+
+
+
+
+// import './App.css';
+// import React from 'react';
+
+// class App extends React.Component {
+
+// state={
+//   count: 0
+// }
+
+// incrementCount = (e) => {
+
+//   this.setState({count: this.state.count + 1})
+// }
+// render(){
+//   let { count } = this.state;
+//   return (
+//     <div className="App">
+//       <button type="button" onClick={this.incrementCount}>I was clicked {this.state.count} times</button>
+//     </div>
+//   ); 
+
+// }
+  
+// }
+
+// export default App;
